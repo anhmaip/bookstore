b488b7b36c6bf68dbbdc365fba8ab1696e150cce
Mai Phan <mai.phan@eastagile.com>
2014-03-11 14:57:36 +0700

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BookstoreMaiphan::Application.config.secret_key_base = '32f6609c2109a549078999c248fe73d809bb4171851bf198a69ac2e90171396c13f3badd489756431f0ced4545318428021c55c35b77224235d102b827245a6f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BookstoreMaiphan::Application.config.session_store :cookie_store, key: '_bookstore_maiphan_session'
