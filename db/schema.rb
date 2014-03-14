07b7ea3dc2291839ca4e04adff2449d9eddd2556
Mai Phan <mai.phan@eastagile.com>
2014-03-14 19:34:09 +0700

    Generate Book model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140314080242) do
+ActiveRecord::Schema.define(version: 20140314123230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "author_name"
+    t.string   "publisher_name"
+    t.date     "published_date"
+    t.integer  "unit_price"
+    t.string   "photo"
+    t.integer  "total_rating_value"
+    t.integer  "total_rating_count"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "categories", force: true do |t|
     t.string   "name"
