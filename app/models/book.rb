8548c57060b1c012ff26d5d99ca2538da2bcf0e3
Mai Phan <mai.phan@eastagile.com>
2014-03-17 14:10:59 +0700

    Add spec to test Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ActiveRecord::Base
+  validates :title, presence: true
+  validates :unit_price, presence: true
+
   has_and_belongs_to_many :categories
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Book do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:book) { FactoryGirl.build(:book) }
+  subject { book }
+
+  it { should be_valid }
+
+  [:title, :description, :author_name, :publisher_name, :published_date,
+   :unit_price, :photo, :total_rating_count, :total_rating_value, :categories].each do |field|
+  it { should respond_to(field) }
+  end
+
+  [:title, :unit_price].each do |field|
+  it { should validate_presence_of(field) }
+  end
+
+  it { should have_and_belong_to_many(:categories) }
 end
