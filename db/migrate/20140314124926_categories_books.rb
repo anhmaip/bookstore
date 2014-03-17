25b29c9c473b15f191a006a6dea689c59325358b
Mai Phan <mai.phan@eastagile.com>
2014-03-17 10:33:53 +0700

    Replace table categories_books by books_categories (wrong join table name)

--- db/migrate/20140314124926_categories_books.rb
+++
@@ -1,8 +0,0 @@
-class CategoriesBooks < ActiveRecord::Migration
-  def change
-    create_table :categories_books, :id => false do |t|
-      t.integer :category_id
-      t.integer :book_id
-    end
-  end
-end
