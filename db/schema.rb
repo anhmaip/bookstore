af4c265d95605c5ca3556256f851556357af4d72
Mai Phan <mai.phan@eastagile.com>
2014-03-18 15:28:43 +0700

    Add default rating = 0

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140317104348) do
+ActiveRecord::Schema.define(version: 20140318072857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20140317104348) do
   end
 
   create_table "comments", force: true do |t|
-    t.integer  "rating"
+    t.integer  "rating",     default: 0
     t.text     "content"
     t.integer  "user_id"
     t.integer  "book_id"
