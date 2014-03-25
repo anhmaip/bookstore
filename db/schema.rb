8dfa890fe8aeb0b37784dacb459ba72beee051bf
Mai Phan <mai.phan@eastagile.com>
2014-03-25 18:03:46 +0700

    Add default to users's admin to false, remove redundant 'role' field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140325084337) do
+ActiveRecord::Schema.define(version: 20140325105721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,8 +99,7 @@ ActiveRecord::Schema.define(version: 20140325084337) do
     t.string   "full_name"
     t.date     "birthday"
     t.string   "phone"
-    t.string   "role"
-    t.boolean  "admin"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
