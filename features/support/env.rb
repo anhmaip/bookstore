cecf92961af111deaae230b8bd24746970e47b55
Mai Phan <mai.phan@eastagile.com>
2014-03-28 10:55:30 +0700

    Setup circle CI test coverage

--- features/support/env.rb
+++ features/support/env.rb
@@ -58,3 +58,8 @@ end
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
+if ENV['CIRCLE_ARTIFACTS']
+  require 'simplecov'
+  dir = File.join("..", "..", "..", ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
